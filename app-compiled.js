7f48d5810f025bf80e75b8ce3e72522cffd46e89
maksimgm <maksimmamrikov@gmail.com>
2017-02-15 15:06:08 -0800

    add sample babel-es6 setup

---
+++ app-compiled.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var age = 100;
+var foods = ["Ice cream", "oreos", "cake"];
+
+var favoriteFoods = foods.map(function (food) {
+  return food + " with whipped cream.";
+});
---
+++ app.js
@@ -0,0 +1,4 @@
+const age = 100;
+const foods = ["Ice cream", "oreos", "cake"];
+
+const favoriteFoods = foods.map(food => `${food} with whipped cream.`)
